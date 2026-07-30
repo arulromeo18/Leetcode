2e707726444cdf69756bcac2b2315374b3d342c3
arulromeo18 <arulkumarv0811@gmail.com>
2026-07-30 16:20:46 +0530

    Time: 3 ms (98.08%), Space: 77.4 MB (29.41%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -3,16 +3,18 @@ class Solution {
         int start=0;
         int end=nums.length-1;
         int max=Integer.MIN_VALUE;
+        int area=1;
         while(start<end){
-            int min=Math.min(nums[start],nums[end]);
-            int area=min*(end-start);
-            max=Math.max(max,area);
+            
             if(nums[start]>nums[end]){
+                area=nums[end]*(end-start);
                 end--;
             }
             else{
+                area=nums[start]*(end-start);
                 start++;
             }
+            if(max<area) max=area;
         }
         return max;
     }
